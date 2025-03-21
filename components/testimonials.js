ae7d9f49949fa09b016bc3d8ffbc98f21ab67a0a
Sarniel Morgia <sarniel@Niels-MBP.gateway>
2025-03-21 13:04:17 +1100

    updates

--- components/testimonials.js
+++ components/testimonials.js
@@ -33,9 +33,6 @@ export function loadTestimonials() {
   document.getElementById('testimonials').innerHTML = `
   <div class="testimonials-section bg-white">
     <div class="container">
-      <div class="text-center">
-        <h2 class="text-uppercase fw-bold mb-5 fs-1">What Our <span class="text-primary">Customers</span> Say</h2>
-      </div>
       <div class="h-100 position-relative">
         <div class="d-flex gap-3 h-100">
           <div class="d-none d-sm-block w-60 position-relative">
@@ -43,35 +40,46 @@ export function loadTestimonials() {
           </div>
 
           <div class="overflow-hidden position-relative">
-            <!-- Left Arrow -->
-            <div class="swiper-button-prev"></div>
-            <!-- Right Arrow -->
-            <div class="swiper-button-next"></div>
-
-            <div class="swiper-container">
-              <div class="swiper-wrapper">
-                ${testimonials
-                  .map(
-                    (testimonial) => `
-                      <div class="swiper-slide">
-                        <div class="testimonial-card bg-light">
-                          <div class="d-flex align-items-center mb-3">
-                            <div class="testimonials-initial">
-                              ${testimonial.initial}
-                            </div>
-                            <div class="ms-3">
-                              <div class="fw-bold fs-5">${testimonial.name}</div>
-                              <div><img src="images/star-icons.png" alt="Ratings" width="135"></div>
-                            </div>
-                          </div>
-                          <div>${testimonial.message}</div>
-                        </div>
-                      </div>`
-                  )
-                  .join('')}
+            <div>
+              <div
+                class="text-uppercase fw-bold text-primary d-flex align-items-center gap-2"
+              >
+                <img src="images/iconNf.png" alt="icon" width="14" />
+                <span>Testimonials</span>
               </div>
+              <h2 class="text-uppercase fw-bold mb-5 fs-1">What Our <span class="text-primary">Customers</span> Say</h2>
             </div>
+            <div class="">
+              <!-- Left Arrow -->
+              <div class="swiper-button-prev"></div>
+              <!-- Right Arrow -->
+              <div class="swiper-button-next"></div>
 
+              <div class="swiper-container">
+                <div class="swiper-wrapper">
+                  ${testimonials
+                    .map(
+                      (testimonial) => `
+                        <div class="swiper-slide">
+                          <div class="testimonial-card bg-light">
+                            <div class="d-flex align-items-center mb-3">
+                              <div class="testimonials-initial">
+                                ${testimonial.initial}
+                              </div>
+                              <div class="ms-3">
+                                <div class="fw-bold fs-5">${testimonial.name}</div>
+                                <div><img src="images/star-icons.png" alt="Ratings" width="135"></div>
+                              </div>
+                            </div>
+                            <div>${testimonial.message}</div>
+                          </div>
+                        </div>`
+                    )
+                    .join('')}
+                </div>
+              </div>
+
+            </div>
           </div>
         </div>
       </div>
