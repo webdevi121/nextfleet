24a1bb8cf6d69ff8bd16232bb903a547a79f9df6
Sarniel Morgia <sarniel@Niels-MBP.gateway>
2025-04-16 11:17:45 +1000

    use kia car

--- php/components/services.php
+++ php/components/services.php
@@ -41,7 +41,7 @@ $services = [
         "buttonLink" => ""
     ],
     [
-      "img" => "images/placeholder-5.png",
+      "img" => "images/kia.png",
       "title" => "Sales & <span class=\"d-block\">Leaseback</span>",
       "description" => "Financing your fleet? That's a lot of valuable capital tied up in depreciating assets. We can free up capital by giving you an instant cash injection by purchasing your existing vehicles.",
       "accentColor" => "accent-color-6",
