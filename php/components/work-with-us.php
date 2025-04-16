aa107267d515877994f791441aaebebb1fd7c68c
Sarniel Morgia <sarniel@Niels-MBP.gateway>
2025-04-16 10:41:03 +1000

    mobile view nextfleet

---
+++ php/components/work-with-us.php
@@ -0,0 +1,39 @@
+<section id="work-with-us" class="bg-black">
+    <div class="container">
+    <div class="row">
+        <div class="col-sm-6 position-relative">
+            <div class="dark-img-thumbnail">
+                <img
+                src="images/woman-driving-img.jpg"
+                alt="thumbnail"
+                class="h-100"
+                />
+            </div>
+        </div>
+        <div class="col-sm-6 ms-auto pt-6 pb-6 text-white">
+            <h2>Why Work With Us?</h2>
+            <p>
+                At NextFleet, we believe in building strong partnerships,
+                delivering exceptional service, and fostering a dynamic,
+                future-focused workplace.
+            </p>
+            <p>
+                For our clients, we provide seamless, end-to-end fleet management
+                solutions, combining expertise, technology, and innovation to
+                drive efficiency and performance.
+            </p>
+            <p>
+                For our partners, we cultivate meaningful, mutually beneficial
+                collaborations to enhance industry standards and deliver
+                best-in-class solutions.
+            </p>
+            <p>
+                And for our employees, we offer an exciting opportunity to shape
+                the future of mobility, working within a diverse, forward-thinking
+                team backed by the global strength of Mitsubishi Corporation.
+            </p>
+            <p>Together, we’re redefining fleet and mobility.</p>
+        </div>
+    </div>
+    </div>
+</section>
