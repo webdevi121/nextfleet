30dbf7d4f5b583029b49974e1e6e7f6987465d1f
Sarniel Morgia <sarniel@Mac.gateway>
2025-08-18 11:50:34 +1000

    fix url path and app icon

--- php/components/mydrive-header.php
+++ php/components/mydrive-header.php
@@ -9,19 +9,19 @@
     <link rel="stylesheet" href="css/style.css" />
     <link rel="stylesheet" href="css/swiper-bundle.min.css" />
     <link rel="stylesheet" href="css/fontawesome.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="icon" type="image/png" href="images/favico.ico" />
+    
     <!-- Standard favicon -->
-    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <link rel="icon" type="image/x-icon" href="images/favicon.ico">
 
     <!-- SVG favicon -->
-    <link rel="icon" type="image/svg+xml" href="favicon.svg">
+    <link rel="icon" type="image/svg+xml" href="images/favicon.svg">
 
     <!-- Android / PWA icons -->
-    <link rel="icon" type="image/png" sizes="192x192" href="web-app-manifest-192x192.png">
-    <link rel="icon" type="image/png" sizes="512x512" href="web-app-manifest-512x512.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="images/web-app-manifest-192x192.png">
+    <link rel="icon" type="image/png" sizes="512x512" href="images/web-app-manifest-512x512.png">
 
     <!-- Apple touch icon -->
-    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
 
     <!-- Manifest -->
     <link rel="manifest" href="site.webmanifest">
